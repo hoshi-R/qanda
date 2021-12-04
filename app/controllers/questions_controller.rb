a1b011c3a9752a8b1437a14fc68bc93db536b85f
hoshi-R <hs.rt7771@gmail.com>
2021-12-04 14:43:38 +0000

    211204

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,25 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: '成功！'
+    else
+      flash[:alert] = '投稿できません'
+      render :new
+    end
   end
 
   def edit
   end
+  
+  private
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end
+    
 end
